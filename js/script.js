ac3e98f1b9c56e7b74758c46af52011da5cec995
vilcinskij <vilcinskij@gmail.com>
2023-03-24 01:58:02 +0200

    pdfGenerationOk

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var docInfo = {
+    info: {
+        title: 'tested PDF',
+        author: 'vilcinskij.com',
+        subject: 'subject',
+        keywords: 'words'
+    },
+    pageSize: 'A4',
+    pageMargins: [50, 50, 30, 60],
+    header: function (currentPage, pageCount) {
+        return {
+            text: currentPage.toString() + 'from' + pageCount,
+            alignment: 'right',
+            margin: [0, 30, 10, 50]
+        }
+    },
+    footer: [
+        {
+            text: 'нижний колонтитул',
+            alignment: 'center'
+        }
+    ],
+    content: [
+        {
+            text: 'ТЕКСТ ПАРАГРАФА'
+        }
+    ]
+}
+
+// pdfMake.createPdf(docInfo).download('name.pdf')
